d986de48cd4251cfb87b782532ab3e759cd5684f
aaron chen <ixdezbtoeck@gmail.com>
2020-07-15 17:36:39 -0700

    Linked List Cycle Done

---
+++ Algorithms_And_LeetCode_Answer/Algorithms_And_LeetCode_Answer/Linked List Cycle.swift
@@ -0,0 +1,64 @@
+//
+//  Linked List Cycle.swift
+//  Algorithms_And_LeetCode_Answer
+//
+//  Created by Aaron Chen on 2020-07-15.
+//  Copyright © 2020 Aaron Chen. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - https://leetcode.com/problems/linked-list-cycle/
+
+/**Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.*/
+
+func hasCycle(_ head: ListNode?) -> Bool {
+    if head == nil{
+        return false
+    }
+    
+    var t = head
+    var r = head?.next
+    
+    while r != nil && r !== t {
+        t = t?.next
+        r = r?.next?.next
+    }
+    return r != nil
+}
+
+/**
+* Definition for singly-linked list.
+* public class ListNode {
+*     public var val: Int
+*     public var next: ListNode?
+*     public init(_ val: Int) {
+*         self.val = val
+*         self.next = nil
+*     }
+* }
+*/
